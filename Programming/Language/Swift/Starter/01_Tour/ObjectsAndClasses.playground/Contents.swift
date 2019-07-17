e0cad0e66f4da75e844a5fc69df19493d287703e
Frank-Tian <tianzhenzi@inke.cn>
2019-07-17 13:39:37 +0800

    Add Enumerations and Structures

--- Programming/Language/Swift/Starter/01_Tour/ObjectsAndClasses.playground/Contents.swift
+++ Programming/Language/Swift/Starter/01_Tour/ObjectsAndClasses.playground/Contents.swift
@@ -157,9 +157,132 @@ let sideLength = optionalSqure?.sideLength
 //////////////////////////////////////////////////////////////////////
 // Enumerations and Structures
 
+/**
+ * Use enum to create an enumeration. Like  classes and all other named types,
+ * enumerations can have methods associated with them
+ */
+
+enum Rank: Int {
+    case ace = 1
+    case two, three, four, five, six, seven, eight, nine, ten
+    case jack, queen, king
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .ace:
+            return "ace"
+        case .queen:
+            return "queen"
+        default:
+            return String(self.rawValue)
+        }
+    }
+}
+
+let ace = Rank.ace
+let aceRawValue = ace.rawValue
+
+print(ace) // ace
+print(aceRawValue) // 1
+print(Rank.jack) // jack
+print(Rank.jack.rawValue) //11
+
+/**
+ * By default, Swift assigns the raw values starting at zero and incrementing by one each time
+ * but you can change this behavior by explicitly specifying values.
+ * You can also use strings or floating-point numbers as the raw type of an enumeration.
+ * Use the rawValue property to access the raw value of an enumeration case.
+ */
+
+if let convertedRank = Rank(rawValue: 3) {
+    let threeDescription = convertedRank.simpleDescription()
+    print(threeDescription)
+}
+
+/**
+ *  The case values of an enumeration are actual values, not just another way of writing their
+ *  raw values. In face, in cases where there isn't a meaningful raw value, you don`t have to provide one
+ */
+enum Suit {
+    case spades, hearts, diamonds, clubs
+    
+    func simpleDescription() -> String {
+        switch self {
+        case .spades:
+            return "spades-"
+        case .hearts:
+            return "hearts-"
+        case .diamonds:
+            return "diamonds-"
+        case .clubs:
+            return "clubs-"
+//        default:
+              // (Error:Value of type 'Suit' has no member 'rawValue')
+//            return String(self.rawValue) // must extend a type.
+        }
+    }
+}
+
+let hearts = Suit.hearts
+let heartsDesc = hearts.simpleDescription()
+print(hearts) // hearts
+print(heartsDesc) // hearts-
 
 
+/**
+ * If an enumeration has raw values, those values are determined as part of the declaration,
+ * which means ervery instance of a particular enumeration case always has the same raw value.
+ * Another choice for enumeration cases is to have values associated with the case-- these values
+ * are determined when you make the instance, and they can be different for each instance of an
+ * enumeration case. You can think of the associated values as behaving like stored properties of the
+ * enumeration case instance. For example, consider the case of requesting the sunrise and sunset times
+ * from a server. The server either responds whith the reqeusted information, or it responds with
+ * a description of what went wrong.
+ */
 
+enum ServerResonse {
+    case result(String, String)
+    case failure(String)
+}
+
+let success = ServerResonse.result("200", "Success")
+let failure = ServerResonse.failure("Something wrong!")
+
+switch success {
+case let .result(sunrise, sunset):
+    print("sunrise is at \(sunrise) and sunset is at \(sunset)")
+case let .failure(message):
+    print("Failure ...\(message)")
+}
+
+switch failure {
+case let .result(code, msg):
+    print("code is \(code) and msg is \(msg)")
+case let .failure(message):
+    print("Failure ...\(message)")
+}
+
+/**
+ * Use struct to create a structure .
+ * Structures support many of the same behaviors as classes,
+ * including methods and initializers. One of the most important differences between structures
+ * and classes is that structures are always copied when they are passed around in your code
+ * but classes are passed by reference.
+ */
+
+struct Card {
+    var rank: Rank
+    var suit: Suit
+    
+    func simpleDescription() -> String {
+        return "The \(rank.simpleDescription()) of \(suit.simpleDescription())"
+    }
+}
+
+let threeOfSpades = Card(rank: .three, suit: .spades)
+let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+print(threeOfSpades) // Card(rank: __lldb_expr_7.Rank.three, suit: __lldb_expr_7.Suit.spades)
+print(threeOfSpadesDescription) // The 3 of spades-
 
 //////////////////////////////////////////////////////////////////////
 // Protocols and Extensions
